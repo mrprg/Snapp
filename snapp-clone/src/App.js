4a7c9f82e09056ae492702b65034141173dc5aab
mrprg <mohamadrezaasadi322@gmail.com>
2023-03-19 19:16:52 +0330

    add Context to project

--- snapp-clone/src/App.js
+++ snapp-clone/src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import NavMenu from "./components/NavMenu";
+import { useStateContext } from "./context/ContextProvider";
 
 function App() {
+  const {activeMenu} = useStateContext()
   return (
     <div>
       <Navbar />
+      {activeMenu && <NavMenu />}
     </div>
   );
 }
