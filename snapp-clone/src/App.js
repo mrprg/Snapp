32843e74dc3aed2f3533b1dc3e9f7ff269a52e4a
mrprg <mohamadrezaasadi322@gmail.com>
2023-03-27 10:20:37 +0430

    add style

--- snapp-clone/src/App.js
+++ snapp-clone/src/App.js
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import NavMenu from "./components/NavMenu";
 import Services from "./components/Services";
+import SuperApp from "./components/SuperApp";
 import { useStateContext } from "./context/ContextProvider";
 
 function App() {
@@ -34,7 +35,9 @@ function App() {
 
   return (
     <>
-      <Navbar />
+      <div className="navbar-position-sticky">
+          <Navbar/>
+      </div>
 
       <Hero />
 
@@ -45,6 +48,7 @@ function App() {
      <div className="food-order-image">
       <img src="https://cdn.donya-e-eqtesad.com/thumbnail/aPUe4kUSCGHH/QHn8O9nsSzT8qCU7RegsN6Pbb5v74eEtbKeSOh05RaY4kX0q0_Y_8Ut7TZyzEhnm/%D8%A7%D8%B3%D9%86%D9%BE.jpg" />
      </div>
+     <SuperApp />
     </>
   );
 }
